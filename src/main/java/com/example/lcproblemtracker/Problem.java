3704df6fd4fb8bfacf3d06b4b066aa82df87761f
Won Yang <yangwon700@gmail.com>
2024-04-15 23:41:21 -0500

    finish creating Problem entity

--- src/main/java/com/example/lcproblemtracker/Problem.java
+++ src/main/java/com/example/lcproblemtracker/Problem.java
@@ -1,17 +1,40 @@
 package com.example.lcproblemtracker;
 
+import java.sql.Date;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Problem {
+    private enum Difficulty {
+        EASY, MEDIUM, HARD
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
     private String[] patterns;
+
     private String link;
+
+    @Enumerated(EnumType.STRING)
+    private Difficulty difficulty;
+
+    private String timeComplexity;
+
+    private String spaceComplexity;
+
+    private String notes;
+
+    private Date lastSolved;
+
+    private int minutesSpent;
 
     public Integer getId() {
         return id;
@@ -35,5 +58,53 @@ public class Problem {
 
     public void setLink(String link) {
         this.link = link;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public String getTimeComplexity() {
+        return timeComplexity;
+    }
+
+    public void setTimeComplexity(String timeComplexity) {
+        this.timeComplexity = timeComplexity;
+    }
+
+    public String getSpaceComplexity() {
+        return spaceComplexity;
+    }
+
+    public void setSpaceComplexity(String spaceComplexity) {
+        this.spaceComplexity = spaceComplexity;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public Date getLastSolved() {
+        return lastSolved;
+    }
+
+    public void setLastSolved(Date lastSolved) {
+        this.lastSolved = lastSolved;
+    }
+
+    public int getMinutesSpent() {
+        return minutesSpent;
+    }
+
+    public void setMinutesSpent(int minutesSpent) {
+        this.minutesSpent = minutesSpent;
     }
 }
